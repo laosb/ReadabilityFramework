2640bebf90b250aa0edc882e919ae59eb83f2f85
Shibo Lyu <github@of.sb>
2022-05-20 11:32:10 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReadabilityFramework
+
+A description of this package.
---
+++ Sources/ReadabilityFramework/ReadabilityFramework.swift
@@ -0,0 +1,6 @@
+public struct ReadabilityFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
